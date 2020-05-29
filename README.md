3debf7d7966d962cd1df33af03f7e23c0acc907c
unknown <liuhh02@gmail.com>
2020-05-30 01:23:52 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# MEAN Stack Emojifier
+
+This repo contains the code for the emojifier web app built using the MEAN stack.
+
+Demo image link:
+
+https://docs.microsoft.com/en-ca/azure/cognitive-services/face/images/facefindsimilar.queryface.jpg
+
+
+MongoDB Windows Startup Command:
+
+`"C:\Program Files\MongoDB\Server\4.2\bin\mongod.exe" --dbpath="D:\data\mongodb"`
---
+++ server/index.ts
@@ -0,0 +1,45 @@
+const express = require('express');
+const cors = require('cors');
+const request = require('request');
+const saveFace = require('./db');
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+  origin: '*',
+}));
+
+app.use(express.static(__dirname + "/../dist/"));
+
+const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect/';
+const subscriptionKey = 'token';
+
+const port = 3000;
+
+app.post('/', (req, res) => {
+  const { imageUrl } = req.body;
+
+  const params = {
+    'returnFaceAttributes': 'emotion'
+  };
+
+  const options = {
+    url: uriBase,
+    qs: params,
+    body: '{"url": ' + '"' + imageUrl + '"}',
+    headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key' : subscriptionKey
+    }
+  };
+
+  request.post(options, (error, response, body) => {
+    console.log(body);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(body);
+  })
+  // TODO: Send Face API response to front-end
+  // TODO: Save Face API response to database
+});
+
+app.listen(port, () => console.log(`Emojifier back-end listening on port ${port}!`));
